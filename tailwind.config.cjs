670ff81200af62c96029010b57b81896bba36e75
scardoso <scardoso@quologistics.com>
2022-12-27 23:10:47 -0600

    Primer commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html', './src/**/*.jsx'],/*Archivos en los que se ocupara tailwindcss */ 
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
